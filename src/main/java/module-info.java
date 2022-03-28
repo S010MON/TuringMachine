fe64bef67ea5111b0e184b9194b13dd7413170d1
S010MON <leon@bdebnath.com>
2022-03-28 11:13:19 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.turing.turingmachine {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.app to javafx.fxml;
+    exports com.app;
+}
